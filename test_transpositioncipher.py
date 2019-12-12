7323e29cb1c61fdaf63a23281f5668884500ae25
Shreeram Murali <54653338+Shreeram38@users.noreply.github.com>
2019-12-12 15:51:20 -0800

    test_trans

---
+++ test_transpositioncipher.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import unittest
+from Secretmsg.Complexencryption import Transpositioncipher
+class Testtranspositioncipher(unittest.TestCase): 
+    def setUp(self):
+        self.p1=Transpositioncipher.Transpositioncipher()
+        self.p2=Transpositioncipher.Transpositioncipher()  
+    def tearDown(self):
+        print('Tear Down')
+
+    def test_tranencrypt(self):         
+        self.assertEqual(self.p1.tranencrypt(3,"hello"),'hleol') 
+        self.assertEqual(self.p2.tranencrypt(3,"help"),'hpel') 
+        self.assertEqual(self.p2.tranencrypt(3,"I am Awesome"),'Imwo  emaAse')
+        self.assertEqual(self.p1.tranencrypt(2,"I am Awesome"),'Ia wsm mAeoe')
+        self.assertNotEqual(self.p1.tranencrypt(3,"I am Awesome"),'L#dp#Dzhvrph')
+    
+    def test_trandecrypt(self):
+        self.assertEqual(self.p1.trandecrypt(3,"hleol"),'hello') 
+        self.assertEqual(self.p2.trandecrypt(3,"hpel"),'help') 
+        self.assertEqual(self.p2.trandecrypt(3,"Imwo  emaAse"),'I am Awesome')
+        self.assertEqual(self.p1.trandecrypt(2,'Ia wsm mAeoe'),'I am Awesome')
+        self.assertNotEqual(self.p1.trandecrypt(3,'L#dp#Dzhvrph'),'I am Awesome')
+    
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass is run only once")
+        
+    @classmethod
+    def tearDownClass(cls):
+        print("Tear Down class")
+        
+
+unittest.main(argv=[''], verbosity=2, exit=False)
+
+
+# In[ ]:
+
+
+
+
