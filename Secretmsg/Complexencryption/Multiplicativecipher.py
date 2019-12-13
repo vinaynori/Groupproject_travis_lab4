27cdfe5f224b624f60cd4b7cc36b0b55e78f07d7
vinaynori <vingovinni@gmail.com>
2019-12-12 18:15:42 -0800

    change

--- Secretmsg/Complexencryption/Multiplicativecipher.py
+++ Secretmsg/Complexencryption/Multiplicativecipher.py
@@ -4,12 +4,10 @@ class Wronginp(Exception):
         print("Exception")
         return None
 
-
-
 class Multiplicativecipher:
     def mulencrypt(self,msg,key):
         try:
-            if type(msg)!= str:
+            if type(msg)!=str:
                 raise(Wronginp("string required"))
             self.msg=msg
             self.key=key
@@ -38,46 +36,45 @@ class Multiplicativecipher:
                         newchar=chr(ord(character)+self.key)
                     newmsg+=newchar
                 return newmsg
-        except inpsize as ex:
-            print("Exception raised",ex.value)
+        except Wronginp as ex:
+            print("Exception raised:",ex.value)
 
     def muldecrypt(self,msg,key):
         try:
-            if type(msg)!= str:
+            if type(msg)!=str:
                 raise(Wronginp("string required"))
 
-            def muldecrypt(self,msg,key):
-                self.msg=msg
-                self.key=key
-                alphabet="abcdefghijklmnopqrstuvwxyz"
-                newmsg=""
-                flag=0
-                if key%2==0 or key==1:
-                    print("key must be odd and greater than 1, within 26")
-                    return -1
-                else:
-                    for character in self.msg:
-                        if character >= 'A' and character <= 'Z':
-                            flag=1
-                        elif character >= 'a' and character <='z':
-                            flag=0
-                        else:
-                            flag=2
-                        if flag!=2:
-                            character=character.lower()
-                            pos=alphabet.find(character)
-                            temp2=-1
-                            for i in range(key,26):
-                                if (key*i) % 26 == 1:
-                                    temp2=i
-                            ans=temp2
-                            newpos=(pos*ans)%26
-                            newchar=alphabet[newpos]
-                            if flag==1:
-                                newchar=newchar.upper()
-                        else:
-                            newchar=chr(ord(character)-self.key)
-                        newmsg+=newchar
-                    return newmsg
-        except inpsize as ex:
-            print("Exception raised",ex.value)
+            self.msg=msg
+            self.key=key
+            alphabet="abcdefghijklmnopqrstuvwxyz"
+            newmsg=""
+            flag=0
+            if key%2==0 or key==1:
+                print("key must be odd and greater than 1, within 26")
+                return -1
+            else:
+                for character in self.msg:
+                    if character >= 'A' and character <= 'Z':
+                        flag=1
+                    elif character >= 'a' and character <='z':
+                        flag=0
+                    else:
+                        flag=2
+                    if flag!=2:
+                        character=character.lower()
+                        pos=alphabet.find(character)
+                        temp2=-1
+                        for i in range(key,26):
+                            if (key*i) % 26 == 1:
+                                temp2=i
+                        ans=temp2
+                        newpos=(pos*ans)%26
+                        newchar=alphabet[newpos]
+                        if flag==1:
+                            newchar=newchar.upper()
+                    else:
+                        newchar=chr(ord(character)-self.key)
+                    newmsg+=newchar
+                return newmsg
+        except Wronginp as ex:
+            print("Exception raised:",ex.value)
