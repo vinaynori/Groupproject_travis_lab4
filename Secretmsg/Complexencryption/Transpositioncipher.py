f2b20f8fc1a7972a2183f84ea53cfc9bb5976c7e
vinaynori <vingovinni@gmail.com>
2019-12-12 18:45:13 -0800

    got

--- Secretmsg/Complexencryption/Transpositioncipher.py
+++ Secretmsg/Complexencryption/Transpositioncipher.py
@@ -2,24 +2,24 @@ import math
 class InsizeError(Exception):
     def __init __(self, value):
         self.value = value
-    def __str __(self): 
+    def __str __(self):
         return(repr(self.value ))
-    
+
 class KeystrError(Exception):
     def __init __(self, value):
         self.value = value
-    def __str __(self): 
+    def __str __(self):
         return(repr(self.value ))
-    
+
 
 class Transpositioncipher:
     def tranencrypt(self,key,msg):
         try:
             if type(key) != int:
-                raise KeystrError("Key is not INT")
+                raise(KeystrError("Key is not INT"))
             if len(msg) <= 1:
-                raise InsizeError("Size of msg is not enough!!")
-      
+                raise(InsizeError("Size of msg is not enough!!"))
+
             self.key=key
             self.msg=msg
             res = [''] * key
@@ -29,20 +29,20 @@ class Transpositioncipher:
                     res[col] += msg[pos]
                     pos += key
             return ''.join(res) #Cipher text
-        
+
         except InsizeError as ex:
             print('Exception raised:',ex.value )
         except KeystrError as ex:
             print('Exception raised:',ex.value )
-            
+
 
 
     def trandecrypt(self,key,msg):
         try:
             if type(key) != int:
-                raise KeystrError("Key is not INT")
+                raise(KeystrError("Key is not INT"))
             if len(msg) <= 1:
-                raise InsizeError("Size of msg is not enough!!")
+                raise(InsizeError("Size of msg is not enough!!"))
             self.key=key
             self.msg=msg
             cols = int(math.ceil(len(msg) / float(key)))
@@ -55,7 +55,7 @@ class Transpositioncipher:
                 res[col] += character
                 col += 1
                 if (col == cols) or (col == cols - 1 and row >= rows - no_t_box):
-                    col = 0 
+                    col = 0
                     row += 1
             return ''.join(res)
         except InsizeError as ex:
--- test_transpositioncipher.py
+++ test_transpositioncipher.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #!/usr/bin/env python
 # coding: utf-8
 
@@ -7,38 +7,38 @@
 
 import unittest
 from Secretmsg.Complexencryption import Transpositioncipher
-class Testtranspositioncipher(unittest.TestCase): 
+class Testtranspositioncipher(unittest.TestCase):
     def setUp(self):
         self.p1=Transpositioncipher.Transpositioncipher()
-        self.p2=Transpositioncipher.Transpositioncipher()  
+        self.p2=Transpositioncipher.Transpositioncipher()
     def tearDown(self):
         print('Tear Down')
 
-    def test_tranencrypt(self):         
-        self.assertEqual(self.p1.tranencrypt(3,"hello"),'hleol') 
-        self.assertEqual(self.p2.tranencrypt(3,"help"),'hpel') 
+    def test_tranencrypt(self):
+        self.assertEqual(self.p1.tranencrypt(3,"hello"),'hleol')
+        self.assertEqual(self.p2.tranencrypt(3,"help"),'hpel')
         self.assertEqual(self.p2.tranencrypt(3,"I am Awesome"),'Imwo  emaAse')
         self.assertEqual(self.p1.tranencrypt(2,"I am Awesome"),'Ia wsm mAeoe')
         self.assertNotEqual(self.p1.tranencrypt(3,"I am Awesome"),'L#dp#Dzhvrph')
         self.assertEqual(self.p1.tranencrypt("Hi","hello"),'Exception raised:Key is not INT')
-        self.assertEqual(self.p1.tranencrypt(3,"h"),'Exception raised:Size of msg is not enough!!')
-    
+        self.assertEqual(self.p1.tranencrypt(3,"h"))
+
     def test_trandecrypt(self):
-        self.assertEqual(self.p1.trandecrypt(3,"hleol"),'hello') 
-        self.assertEqual(self.p2.trandecrypt(3,"hpel"),'help') 
+        self.assertEqual(self.p1.trandecrypt(3,"hleol"),'hello')
+        self.assertEqual(self.p2.trandecrypt(3,"hpel"),'help')
         self.assertEqual(self.p2.trandecrypt(3,"Imwo  emaAse"),'I am Awesome')
         self.assertEqual(self.p1.trandecrypt(2,'Ia wsm mAeoe'),'I am Awesome')
         self.assertNotEqual(self.p1.trandecrypt(3,'L#dp#Dzhvrph'),'I am Awesome')
         self.assertEqual(self.p1.trandecrypt("Hi","hello"),'Exception raised:Key is not INT')
-        self.assertEqual(self.p1.trandecrypt(3,"h"),'Exception raised:Size of msg is not enough!!')
+        self.assertEqual(self.p1.trandecrypt(3,"h"))
     @classmethod
     def setUpClass(cls):
         print("setUpClass is run only once")
-        
+
     @classmethod
     def tearDownClass(cls):
         print("Tear Down class")
-        
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
 
@@ -57,35 +57,35 @@ unittest.main(argv=[''], verbosity=2, exit=False)
 
 import unittest
 from Secretmsg.Complexencryption import Transpositioncipher
-class Testtranspositioncipher(unittest.TestCase): 
+class Testtranspositioncipher(unittest.TestCase):
     def setUp(self):
         self.p1=Transpositioncipher.Transpositioncipher()
-        self.p2=Transpositioncipher.Transpositioncipher()  
+        self.p2=Transpositioncipher.Transpositioncipher()
     def tearDown(self):
         print('Tear Down')
 
-    def test_tranencrypt(self):         
-        self.assertEqual(self.p1.tranencrypt(3,"hello"),'hleol') 
-        self.assertEqual(self.p2.tranencrypt(3,"help"),'hpel') 
+    def test_tranencrypt(self):
+        self.assertEqual(self.p1.tranencrypt(3,"hello"),'hleol')
+        self.assertEqual(self.p2.tranencrypt(3,"help"),'hpel')
         self.assertEqual(self.p2.tranencrypt(3,"I am Awesome"),'Imwo  emaAse')
         self.assertEqual(self.p1.tranencrypt(2,"I am Awesome"),'Ia wsm mAeoe')
         self.assertNotEqual(self.p1.tranencrypt(3,"I am Awesome"),'L#dp#Dzhvrph')
-    
+
     def test_trandecrypt(self):
-        self.assertEqual(self.p1.trandecrypt(3,"hleol"),'hello') 
-        self.assertEqual(self.p2.trandecrypt(3,"hpel"),'help') 
+        self.assertEqual(self.p1.trandecrypt(3,"hleol"),'hello')
+        self.assertEqual(self.p2.trandecrypt(3,"hpel"),'help')
         self.assertEqual(self.p2.trandecrypt(3,"Imwo  emaAse"),'I am Awesome')
         self.assertEqual(self.p1.trandecrypt(2,'Ia wsm mAeoe'),'I am Awesome')
         self.assertNotEqual(self.p1.trandecrypt(3,'L#dp#Dzhvrph'),'I am Awesome')
-    
+
     @classmethod
     def setUpClass(cls):
         print("setUpClass is run only once")
-        
+
     @classmethod
     def tearDownClass(cls):
         print("Tear Down class")
-        
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
 
