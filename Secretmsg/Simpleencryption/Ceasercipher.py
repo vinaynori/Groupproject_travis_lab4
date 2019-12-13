e1e72a7a1844d0565757c51a529464f49fef5c4f
vinaynori <vingovinni@gmail.com>
2019-12-12 17:18:21 -0800

    third

--- Secretmsg/Simpleencryption/Ceasercipher.py
+++ Secretmsg/Simpleencryption/Ceasercipher.py
@@ -1,54 +1,73 @@
+class Wronginp(Exception):
+    def __init__(self,value):
+        self.value=value
+    def __str__(self):
+        return (repr(self.value))
+
 
 class Ceasercipher:
     def __init__(self,key):
         self.key=key
 
     def ceaserencrypt(self,msg):
-        self.msg=msg
-        alphabet="abcdefghijklmnopqrstuvwxyz"
-        newmsg=""
-        flag=0
-        for character in self.msg:
-            if character >= 'A' and character <= 'Z':
-                flag=1
-            elif character >= 'a' and character <='z':
-                flag=0
-            else:
-                flag=2
-            if flag!=2:
-                character=character.lower()
-                pos=alphabet.find(character)
-                newpos=(pos+self.key)%26
-                newchar=alphabet[newpos]
-                if flag==1:
-                    newchar=newchar.upper()
-            else:
-                newchar=chr(ord(character)+self.key)
-            newmsg+=newchar
-        return newmsg
+        try:
+            if type(msg)!= str:
+                raise(Wronginp("string required"))
+            self.msg=msg
+            alphabet="abcdefghijklmnopqrstuvwxyz"
+            newmsg=""
+            flag=0
+            for character in self.msg:
+                if character >= 'A' and character <= 'Z':
+                    flag=1
+                elif character >= 'a' and character <='z':
+                    flag=0
+                else:
+                    flag=2
+                if flag!=2:
+                    character=character.lower()
+                    pos=alphabet.find(character)
+                    newpos=(pos+self.key)%26
+                    newchar=alphabet[newpos]
+                    if flag==1:
+                        newchar=newchar.upper()
+                else:
+                    newchar=chr(ord(character)+self.key)
+                newmsg+=newchar
+            return newmsg
+        except Wronginp as ex:
+            print("Exception raised",ex.value)
+
     def ceaserdecrypt(self,msg):
-        self.msg=msg
-        alphabet="abcdefghijklmnopqrstuvwxyz"
-        newmsg=""
-        flag=0
-        for character in self.msg:
-            if character >= 'A' and character <= 'Z':
-                flag=1
-            elif character >= 'a' and character <='z':
-                flag=0
-            else:
-                flag=2
-            if flag!=2:
-                character=character.lower()
-                pos=alphabet.find(character)
-                newpos=(pos-self.key)%26
-                newchar=alphabet[newpos]
-                if flag==1:
-                    newchar=newchar.upper()
-            else:
-                newchar=chr(ord(character)-self.key)
-            newmsg+=newchar
-        return newmsg
+        try:
+            self.msg=msg
+            if type(self.msg)!= str:
+                raise(Wronginp("string required"))
+            alphabet="abcdefghijklmnopqrstuvwxyz"
+            newmsg=""
+            flag=0
+            for character in self.msg:
+                if character >= 'A' and character <= 'Z':
+                    flag=1
+                elif character >= 'a' and character <='z':
+                    flag=0
+                else:
+                    flag=2
+                if flag!=2:
+                    character=character.lower()
+                    pos=alphabet.find(character)
+                    newpos=(pos-self.key)%26
+                    newchar=alphabet[newpos]
+                    if flag==1:
+                        newchar=newchar.upper()
+                else:
+                    newchar=chr(ord(character)-self.key)
+                newmsg+=newchar
+            return newmsg
+        except Wronginp as ex:
+            print("Exception raised",ex.value)
+
+
 
 ##Description
 ##Each letter of plain text is replaced by a letter with some fixed number of positions down with alphabet.
--- Secretmsg/Simpleencryption/Reversecipher.py
+++ Secretmsg/Simpleencryption/Reversecipher.py
@@ -1,15 +1,33 @@
-class Reversecipher: 
-        
+class inpsize(Exception):
+    def __init__(self,value):
+        self.value=value
+    def __str__(self):
+        return (repr(self.value))
+
+class Reversecipher:
+
     def revencrypt(self,msg):
-        self.msg=msg
-        res=self.msg[::-1]
-        return res
-    
+        try:
+            if len(msg)<=1:
+                raise(inpsize("string length greter than 1 required"))
+            self.msg=msg
+            res=self.msg[::-1]
+            return res
+        except inpsize as ex:
+            print("Exception raised",ex.value)
+
     def revdecrypt(self,msg):
-        self.msg=msg
-        res=self.msg[::-1]
-        return res
-        
+        try:
+            if len(msg)<=1:
+                raise(inpsize("string length greter than 1 required"))
+            self.msg=msg
+            res=self.msg[::-1]
+            return res
+        except inpsize as ex:
+            print("Exception raised",ex.value)
+    
+
+
 ##Description:
 ##Inherits features of both Ceasercipher and Doublecipher .
 ##Eg:- HELLO -> OLLEH -> PMMFI
