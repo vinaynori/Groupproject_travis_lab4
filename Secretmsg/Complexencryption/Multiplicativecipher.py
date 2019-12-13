6fb9e35356e13899a7fefec19818e9211e4fca3d
vinaynori <vingovinni@gmail.com>
2019-12-12 18:02:40 -0800

    change

--- Secretmsg/Complexencryption/Multiplicativecipher.py
+++ Secretmsg/Complexencryption/Multiplicativecipher.py
@@ -1,8 +1,8 @@
 class Wronginp(Exception):
     def __init__(self,value):
         self.value=value
-    def __str__(self):
-        return (repr(self.value))
+        print("Exception")
+        return None
 
 
 
